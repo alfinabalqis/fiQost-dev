ca00cb417884f95462c2c7d64c3918778ef3a38a
alfinabalqis <alfinabalqis28@gmail.com>
2023-09-30 13:34:38 +0700

    styling navbar, leftbar, dashboard

--- src/components/layout/Baselayout.jsx
+++ src/components/layout/Baselayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import Dashboard from '@/pages/Dashboard';
 import LeftBar from '../common/LeftBar';
-import RightBar from '../common/RightBar';
 import Navbar from '../common/Navbar';
 
 export default function BaseLayout({ children }) {
@@ -11,15 +11,14 @@ export default function BaseLayout({ children }) {
     <div className="relative">
       {
         location.pathname !== '/' && location.pathname !== '/register' && (
-        <header className="flex flex-row justify-between">
+        <header className="flex flex-row">
           <LeftBar />
           <Navbar />
-          <RightBar />
         </header>
         )
       }
 
-      <div className="absolute left-0 flex justify-center w-full">
+      <div className="">
         {children}
       </div>
     </div>
